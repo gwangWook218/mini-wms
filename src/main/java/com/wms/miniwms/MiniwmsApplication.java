fb4323a017614e3e3979e4a2a5c8e013bd8f2ee8
KwangWook An <akw2080@naver.com>
2026-05-24 23:34:47 +0900

    chore: 프로젝트 초기 생성

---
+++ src/main/java/com/wms/miniwms/MiniwmsApplication.java
@@ -0,0 +1,13 @@
+package com.wms.miniwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniwmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniwmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wms/miniwms/MiniwmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wms.miniwms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniwmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
